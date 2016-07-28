a5e25bbe7657aa682c77c1d854901408f5d2626f
Mariza Pereira <marupereira@ladmins-macbook.attlocal.net>
2016-07-27 22:23:44 -0700

    First commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ function myFunction() {
     var x = document.getElementById("mySelect").value;
     document.getElementById("yourSign").innerHTML = x;
 }
+
+/*function myHoroPicture(){
+	var y = document.getElementById("mySelect").value;
+	if (y = "<span>Aries</span>: your gifts include courage and being independent.") {
+		document.getElementById("horoscopePics").innerHTML = "img/signPictures/aries.png"
+	}
+}*/
